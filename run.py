239c302fff0fb6c1fdeafce033cf0c4e03affd13
Ruan Maia <ruancarlos.maia@gmail.com>
2021-01-04 16:27:12 -0300

    wip

--- run.py
+++ run.py
@@ -10,8 +10,8 @@ from loguru import logger
 @click.option('-h', '--host', required=True)
 @click.option('--dry-run', is_flag=True)
 @click.password_option('-p', '--password', confirmation_prompt=False)
-@click.option('-P', '--pattern')
-@click.option('-S', '--state')
+@click.option('-S', '--state', default=None)
+@click.option('-P', '--pattern', default=None)
 def run(username, password, database, host, pattern, state, dry_run):
     
     cnx = mysql.connector.connect(
@@ -27,10 +27,13 @@ def run(username, password, database, host, pattern, state, dry_run):
             (database,)
         )
         
+        print(pattern)
+        print(state)
+
         for _id, row_state, info in cursor:
             if pattern and info is not None:
                 if info.lower().startswith(pattern.lower()):
-                    print(_id, state, info)
+                    print(_id, row_state, info)
                     if not dry_run:
                         try:
                             kill_cs = cnx.cursor()
@@ -38,17 +41,18 @@ def run(username, password, database, host, pattern, state, dry_run):
                             kill_cs.close()
                         except:
                             pass
-            if state:
-                if row_state.lower().strip() == state.lower().strip():
-                    print(_id, state, info)
-                    if not dry_run:
-                        try:
-                            kill_cs = cnx.cursor()
-                            kill_cs.execute("KILL {};".format(_id))
-                            kill_cs.close()
-                        except:
-                            pass
-            
+            else:
+                if state and row_state is not None:
+                    if row_state.lower().strip() == state.lower().strip():
+                        print(_id, row_state, info)
+                        if not dry_run:
+                            try:
+                                kill_cs = cnx.cursor()
+                                kill_cs.execute("KILL {};".format(_id))
+                                kill_cs.close()
+                            except:
+                                pass
+                
         cursor.close()
     except:
         logger.exception('what?')
